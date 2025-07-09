0b7aa664745f2784c04713bdb57076b259ef9fcb
wuqiuyang <wuqiuyang@baidu.com>
2025-07-09 21:07:56 +0800

    chore: update tsconfig.ts

--- src/utils/packages/auto-reporter/src/__tests__/index.test.ts
+++ src/utils/packages/auto-reporter/src/__tests__/index.test.ts
@@ -1,8 +1,8 @@
 import { describe, expect, it } from '@jest/globals';
-import { auto-reporter } from '../index';
+import { AutoReporter } from '../index';
 
 describe('utils', () => {
   it('should export utilities', () => {
-    expect(auto-reporter).toBeDefined();
+    expect(AutoReporter).toBeDefined();
   });
 });
