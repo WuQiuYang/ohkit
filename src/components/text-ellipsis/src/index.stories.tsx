eed8a18613ca1d9fd2c84489a6ea9fea4f285c65
wuqiuyang <wuqiuyang@baidu.com>
2025-07-23 14:13:29 +0800

    feat: [comp] update text-ellipsis

--- src/components/text-ellipsis/src/index.stories.tsx
+++ src/components/text-ellipsis/src/index.stories.tsx
@@ -1,8 +1,9 @@
+import {useState, useCallback} from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import { Button } from 'antd';
+import { Button, Tooltip, Popover } from 'antd';
 import { DownOutlined, UpOutlined } from '@ant-design/icons';
 
-import { TextEllipsis } from '.';
+import { TextEllipsis, TextEllipsisProps } from '.';
 import { text, element } from './__test__/data';
 
 const meta = {
@@ -25,14 +26,16 @@ export const RightCustom: Story = {
   args: {
     lines: 2,
     content: text,
+    showTitleWhenFold: true,
     renderFoldButton: (fold) => {
       return <Button
-        size='middle'
-        type='text'
+        size='small'
+        type='link'
         color='primary'
+        iconPosition='end'
         icon={fold ? <DownOutlined /> : <UpOutlined />}
       >
-        自定义{fold ? '展开' : '收起'}按钮
+        自定义{fold ? '展开' : '收起'}
       </Button>;
     }
   }
@@ -42,7 +45,10 @@ export const Bottom: Story = {
   args: {
     lines: 2,
     uiType: 'bottom',
+    showTitleWhenFold: true,
+    titleWhenFold: '如果太长了hover我也就不展示全了，可自定义title文案',
     content: text,
+    lineHeight: '22px',
   }
 };
 
@@ -51,5 +57,93 @@ export const RichText: Story = {
     lines: 7,
     uiType: 'bottom',
     content: element,
+  }
+};
+
+export const CustomWithTooltip: Story = {
+  render: (args) => {
+    const [visible, setVisible] = useState(false);
+    const [title, setTitle] = useState('');
+    const handleVisibleChange = useCallback((newVisible: boolean) => {
+      console.log('handleVisibleChange newVisible', newVisible);
+      setVisible(newVisible);
+    }, []);
+    const handleStatusChange = useCallback<NonNullable<TextEllipsisProps['onStatusChange']>>((status) => {
+      console.log('触发了 onStatusChange 回调', status);
+      const {ellipsis, fold, title} = status;
+      if (ellipsis && fold && title) {
+        setTitle(title);
+      } else {
+        setTitle('');
+        setVisible(false);
+      }
+    }, []);
+    return (
+      <Tooltip
+        title={<div style={{maxHeight: 350, overflowY: 'auto'}}>{title}</div>}
+        open={visible}
+        onOpenChange={title ? handleVisibleChange : undefined}
+        placement='bottomRight'
+        mouseEnterDelay={0.3}
+      >
+        <TextEllipsis {...args} onStatusChange={handleStatusChange} />
+      </Tooltip>
+    )
+  },
+  args: {
+    lines: 2,
+    uiType: 'right',
+    content: '适合不是特别长的文本，太长出滚动条比较丑:\n' + text,
+    lineHeight: '22px',
+    onFoldChange: (fold) => {
+      console.log('触发了 onFoldChange 回调 fold', fold);
+    },
+    onEllipsisChange: (ellipsis) => {
+      console.log('触发了 onEllipsisChange 回调 ellipsis', ellipsis);
+    },
+  }
+};
+
+export const CustomWithPopover: Story = {
+  render: (args) => {
+    const [visible, setVisible] = useState(false);
+    const [title, setTitle] = useState('');
+    const handleVisibleChange = useCallback((newVisible: boolean) => {
+      setVisible(newVisible);
+    }, []);
+    const handleStatusChange = useCallback<NonNullable<TextEllipsisProps['onStatusChange']>>((status) => {
+      console.log('触发了 onStatusChange 回调', status);
+      const {ellipsis, fold, title} = status;
+      if (ellipsis && fold && title) {
+        setTitle(title);
+      } else {
+        setTitle('');
+        setVisible(false);
+      }
+    }, []);
+    return (
+      <Popover
+        title={<div style={{maxHeight: 300, maxWidth: 400, overflowY: 'auto'}}>{title}</div>}
+        open={visible}
+        onOpenChange={title ? handleVisibleChange : undefined}
+        placement='bottomRight'
+        mouseEnterDelay={0.3}
+      >
+        <TextEllipsis {...args} onStatusChange={handleStatusChange} />
+      </Popover>
+    )
+  },
+  args: {
+    lines: 2,
+    uiType: 'right',
+    showFoldControl: false,
+    content: '适合超长文本，出滚动条:\n' + text + text + text,
+    lineHeight: '22px',
+    onFoldChange: (fold) => {
+      console.log('触发了 onFoldChange 回调 fold', fold);
+    },
+    onEllipsisChange: (ellipsis) => {
+      console.log('触发了 onEllipsisChange 回调 ellipsis', ellipsis);
+    },
   }
 };
--- src/components/text-ellipsis/src/index.tsx
+++ src/components/text-ellipsis/src/index.tsx
@@ -5,6 +5,7 @@
  */
 
 import React, {
+  forwardRef,
   useState,
   useMemo,
   useEffect,
@@ -21,12 +22,22 @@ import {
   assignRef,
   useRuntime,
   useCompatibleEffect,
+  useSyncPropsState,
 } from "@ohkit/utils";
 import { Measure } from "@ohkit/measure";
 
 export const c = p("ohkit-text-ellipsis__");
 
-interface ITextEllipsis {
+interface ITextEllipsis
+  extends Pick<
+    React.DOMAttributes<HTMLDivElement>,
+    | "onMouseEnter"
+    | "onMouseLeave"
+    | "onPointerEnter"
+    | "onPointerLeave"
+    | "onFocus"
+    | "onClick"
+  > {
   /**
    * right | bottom 展开按钮在右下侧还是底部
    * @default right
@@ -51,10 +62,20 @@ interface ITextEllipsis {
    */
   content?: React.ReactNode;
   /**
+   * 折叠状态
+   * @default true
+   */
+  fold?: boolean;
+  /**
    * 显示展开控制按钮
    * @default true
    */
   showFoldControl?: boolean;
+  /**
+   * 展开按钮位置 uiType='bottom'时有效
+   * @default center
+   */
+  controlPlacement?: 'left' | 'center' | 'right';
   /**
    * 展开按钮文字
    * @default 收起
@@ -65,6 +86,15 @@ interface ITextEllipsis {
    * @default 展开
    */
   unfoldText?: string;
+  /**
+   * 折叠状态下是否显示title属性
+   * @default false
+   */
+  showTitleWhenFold?: boolean;
+  /**
+   * 折叠状态自定义title属性内容
+   */
+  titleWhenFold?: string | ((title: string) => string);
   /**
    * 自定义渲染展开按钮
    */
@@ -77,60 +107,69 @@ interface ITextEllipsis {
    * @param ellipsis 是否截断，true 截断，false 未截断
    */
   onEllipsisChange?: (ellipsis: boolean) => void;
+  /**
+   * 关键状态变更触发
+   * @param status
+   */
+  onStatusChange?: (status: {
+    fold: boolean;
+    ellipsis: boolean;
+    title?: string;
+  }) => void;
 }
 
 export type TextEllipsisProps = PropsWithChildren<ITextEllipsis>;
 
-export function TextEllipsis({
-  className,
-  /**
-   * （单位:px）未传入或无效（0也视为无效）则自动取当前文本的行高
-   */
-  lineHeight = "",
-  /**
-   * 超过几行折叠(number > 0), 没传或者传入无效值不限制，自动截断到容器的最大高度
-   */
-  lines,
-  /**
-   * 展开按钮蒙层背景色(仅支持16进制表示)
-   */
-  maskBgColor = "#fff",
-  /**
-   * text｜ReactNode  与children任传一个
-   */
-  content,
-  children,
-  /**
-   * 显示展开控制按钮
-   */
-  showFoldControl = true,
-  foldText = "收起",
-  unfoldText = "展开",
-  /**
-   * right | bottom 展开按钮在右下侧还是底部
-   */
-  uiType = "right",
-  renderFoldButton,
-  onEllipsisChange,
-  onFoldChange,
-}: TextEllipsisProps) {
+export const TextEllipsis = forwardRef<HTMLDivElement, TextEllipsisProps>((props, ref) => {
+  const {
+    className,
+    lineHeight = "",
+    lines,
+    maskBgColor = "#fff",
+    content,
+    children,
+    showTitleWhenFold,
+    titleWhenFold,
+    showFoldControl = true,
+    foldText = "收起",
+    unfoldText = "展开",
+    uiType = "right",
+    controlPlacement = 'center',
+    renderFoldButton,
+    onEllipsisChange,
+    onFoldChange,
+    onStatusChange,
+    onMouseEnter,
+    onMouseLeave,
+    onPointerEnter,
+    onPointerLeave,
+    onClick,
+    onFocus,
+  } = props;
   // 是否截断
   const [ellipsis, setEllipsis] = useState(false);
   const [getLineHeightFail, setGetLineHeightFail] = useState(false);
   // 折叠状态
-  const [fold, setFold] = useState(true);
+  const [fold, setFold] = useSyncPropsState(props, 'fold', {defaultValue: true, onChange: onFoldChange});
   const [foldBtnWidth, setFoldBtnWidth] = useState(1);
-  const [innerLineHeight, setInnerLineHeight] = useState(0);
+  const [innerLineHeight, setInnerLineHeight] = useState(
+    typeof lineHeight === "string" && lineHeight.endsWith("px")
+      ? parseFloat(lineHeight)
+      : 0
+  );
   const [innerLines = 0, setInnerLines] = useState(lines);
+  // children提取的纯文本
+  const [textContent, setTextContent] = useState('');
 
   const [runtime] = useRuntime({
     contentOffsetHeight: 0,
     ellipsis,
     fold,
     foldBtnWidth,
+    textContent,
     onEllipsisChange,
     onFoldChange,
-  }, ['onEllipsisChange', 'onFoldChange']);
+  }, ['onEllipsisChange', 'fold', 'onFoldChange']);
 
   const contentRef = useRef<HTMLDivElement>(null);
   const wrapperRef = useRef<HTMLDivElement>(null);
@@ -139,12 +178,9 @@ export function TextEllipsis({
 
   const containerStyle = useMemo(() => {
     return {
-      lineHeight: lineHeight
-        ? lineHeight
-        : // 未传入且获取 lineHeight(px) 失败，则设置 default lineHeight: 1.5(em)
-        getLineHeightFail
-        ? "1.5"
-        : undefined,
+      lineHeight: getLineHeightFail // 未传入且获取 lineHeight(px) 失败，则设置 default lineHeight: 1.4(em)
+        ? "1.4" // more brower normal default lineHeight
+        : lineHeight ? lineHeight : undefined,
     };
   }, [lineHeight, getLineHeightFail]);
 
@@ -158,7 +194,9 @@ export function TextEllipsis({
       // HACK: 兼容safari 15+ 富文本折叠高度丢失问题
       minHeight: fold ? `${(lines - 0.5) * innerLineHeight}px` : undefined,
       WebkitLineClamp: fold ? lines : undefined, // 利用-webkit-line-clamp截断方案
-      // maxHeight: ellipsis && fold && lines * innerLineHeight, // TODO: 上面方案不支持 则需优雅降级为高度截断方案
+      // Note: safari 对WebkitLineClamp支持太差劲 判断浏览器优雅降级为高度截断方案
+      // WebkitLineClamp: isSafari ? undefined :  ellipsis && fold && lines, // 利用-webkit-line-clamp截断方案
+      // maxHeight: isSafari && ellipsis && fold ? lines * innerLineHeight : undefined,
       paddingBottom:
         uiType === "bottom" || !fold ? `${innerLineHeight}px` : undefined,
     };
@@ -172,7 +210,7 @@ export function TextEllipsis({
     // 按钮padding，取行高
     const padding = innerLineHeight;
     // 蒙层透明度所占比例
-    const ratio = uiType === "right" ? (padding / foldBtnWidth) * 100 : 60;
+    const ratio = uiType === "right" ? Math.min((padding / foldBtnWidth) * 100, 80) : 60;
     // 16进制透明色(考虑简写方式), 不直接使用css的transparent是因为safari的表现是灰色
     const transparent = `${maskBgColor}${
       maskBgColor.length === 4 ? "0" : "00"
@@ -199,16 +237,20 @@ export function TextEllipsis({
     }
   }, []);
 
-  const handleFoldChange = useCallback((evt?: MouseEvent<HTMLDivElement>, fold = !runtime.fold) => {
-    runtime.fold = fold;
-    setFold(fold);
-    runtime.onFoldChange?.(fold);
+  const handleFoldChange = useCallback(
+    (evt?: MouseEvent<HTMLDivElement>, fold = !runtime.fold) => {
+      runtime.fold = fold;
+      setFold(fold);
   }, []);
 
   const ButtonComp = useMemo(() => {
     return (
       <div
-        className={cx("btn-fold-wrapper", `btn-fold-wrapper-${uiType}`)}
+        className={cx(
+          "btn-fold-wrapper",
+          `btn-fold-wrapper-${uiType}`,
+          uiType === "bottom" && `placement-${controlPlacement}`
+        )}
         style={btnStyle}
         ref={btnWrapperRef}
         onClick={handleFoldChange}
@@ -216,9 +258,7 @@ export function TextEllipsis({
         {renderFoldButton ? (
           renderFoldButton(fold)
         ) : (
-          <div className={"btn-fold"}>
-            {fold ? unfoldText : foldText}
-          </div>
+          <div className={"btn-fold"}>{fold ? unfoldText : foldText}</div>
         )}
       </div>
     );
@@ -228,20 +268,20 @@ export function TextEllipsis({
     foldText,
     handleFoldChange,
     renderFoldButton,
-    // renderIcon,
+    controlPlacement,
     uiType,
     unfoldText,
   ]);
 
   // 重置状态
-  const resetState = useCallback((ellipsis = false) => {
-    const {ellipsis: preEllipsis, fold: preFold} = runtime;
-    if (ellipsis !== preEllipsis) {
-      runtime.ellipsis = ellipsis;
-      setEllipsis(ellipsis);
-      runtime.onEllipsisChange?.(ellipsis);
+  const resetState = useCallback((newEllipsis = runtime.ellipsis) => {
+    const {ellipsis, fold: preFold} = runtime;
+    if (newEllipsis !== ellipsis) {
+      setEllipsis(newEllipsis);
+      runtime.ellipsis = newEllipsis;
+      runtime.onEllipsisChange?.(newEllipsis);
       // 从未截断状态切换为截断状态时，自动折叠（即：出现展开按钮）
-      if (ellipsis && !preFold) {
+      if (newEllipsis && !preFold) {
         handleFoldChange(undefined, true);
       }
     }
@@ -275,13 +315,17 @@ export function TextEllipsis({
     if (!lines) {
       if (runtime.contentOffsetHeight > containerDom?.offsetHeight) {
           const adjustLines = Math.floor(containerDom.offsetHeight / realLineHeight);
-          setInnerLines(adjustLines);
+          if (innerLines !== adjustLines) {
+            setInnerLines(adjustLines);
+          }
           resetState(true);
       } else {
         resetState(false);
       }
     } else {
-      setInnerLines(lines);
+      if (innerLines !== lines) {
+        setInnerLines(lines);
+      }
       if (runtime.contentOffsetHeight >= (lines + 1) * realLineHeight) {
         resetState(true);
       } else {
@@ -300,36 +344,69 @@ export function TextEllipsis({
   // 监听"展开"按钮宽度变化
   useEffect(() => {
     if (ellipsis && btnWrapperRef.current) {
-      const {offsetWidth} = btnWrapperRef.current;
+      const {offsetWidth, offsetHeight} = btnWrapperRef.current;
       if (offsetWidth !== runtime.foldBtnWidth) {
         runtime.foldBtnWidth = offsetWidth;
         setFoldBtnWidth(offsetWidth);
       }
     }
-  }, [ellipsis, unfoldText]);
+  }, [ellipsis, unfoldText, showFoldControl]);
   useEffect(() => {
     if (isSafari) {
       reorganizeDom();
     }
   }, [fold, reorganizeDom]);
+  const updateTextContent = useCallback(() => {
+    const newTextContent = wrapperRef.current?.textContent || '';
+    if (newTextContent !== runtime.textContent) {
+      runtime.textContent = newTextContent;
+      setTextContent(newTextContent);
+    }
+  }, []);
   const finalContent = content || children;
   const hoverTitle = useMemo(() => {
-      return ellipsis && fold ? wrapperRef.current?.textContent || '' : undefined;
-  }, [ellipsis, fold]);
-  // console.log('[render TextEllipsis]: ellipsis, fold: ', ellipsis, fold);
+      return ellipsis && fold
+        ? (typeof titleWhenFold === 'function'
+          ? titleWhenFold(textContent)
+          : titleWhenFold || textContent)
+        : undefined;
+  }, [titleWhenFold, ellipsis, fold, textContent]);
+  useEffect(() => {
+      onStatusChange?.({
+          ellipsis,
+          fold,
+          title: hoverTitle
+      });
+  }, [onStatusChange, fold, ellipsis, hoverTitle]);
+  console.log('[render TextEllipsis]: ellipsis fold: ', ellipsis, fold);
   return (
-    <div className={cx(c("container"), className)} style={containerStyle} ref={containerRef}>
+    <div
+      className={cx(c("container"), className)}
+      style={containerStyle}
+      ref={(r) => {
+        assignRef(containerRef, r);
+        ref && assignRef(ref, r);
+      }}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      onPointerEnter={onPointerEnter}
+      onPointerLeave={onPointerLeave}
+      onClick={onClick}
+      onFocus={onFocus}
+    >
       {/* 此dom仅用于计算高度 用.text-ellipsis-inner计算 在不重新初始化情况下切换文本时高度计算有问题 */}
       <Measure offset>
         {({measureRef, contentRect}) => {
           // console.log('contentRect:', contentRect.offset?.height, runtime.contentOffsetHeight);
           const {height} = contentRect.offset || {};
           if (height !== undefined && Math.abs(height - runtime.contentOffsetHeight) > 1) {
+            console.log('calcEllipsis');
             calcEllipsis();
           }
           return <div className={"offset-height-computer"} ref={(r) => {
             assignRef(measureRef, r);
             assignRef(wrapperRef, r);
+            updateTextContent();
           }}>
             {finalContent}
           </div>
@@ -339,7 +416,12 @@ export function TextEllipsis({
           {finalContent}
       </div> */}
       {/* 主文本显示 */}
-      <div className={"text-ellipsis-inner"} title={hoverTitle} style={wrapStyle} ref={contentRef}>
+      <div
+        className={"text-ellipsis-inner"}
+        title={showTitleWhenFold ? hoverTitle : undefined}
+        style={wrapStyle}
+        ref={contentRef}
+      >
         {/* {finalContent} */}
         {/* firefox >= 133 绝对定位的按钮放文本后面也会被截断隐藏！！ , 放文本前面可解决 */}
         {ellipsis && showFoldControl && ButtonComp}
@@ -347,5 +429,4 @@ export function TextEllipsis({
       </div>
     </div>
   );
-}
-
+});
