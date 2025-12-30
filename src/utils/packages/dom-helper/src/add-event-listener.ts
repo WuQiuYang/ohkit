6abecdcff92bb4b724452224afd030bc4558130c
wuqiuyang <wuqiuyang@baidu.com>
2025-12-30 21:38:17 +0800

    feat: utils update dom-helper

--- src/utils/packages/dom-helper/src/add-event-listener.ts
+++ src/utils/packages/dom-helper/src/add-event-listener.ts
@@ -7,7 +7,7 @@
  * @param options 可选参数，表示是否使用捕获模式或添加事件监听器的其他选项
  * @returns 一个函数，用于移除添加的事件监听器
  */
-export function addEventListener(dom: Window | Document, type: string, cb: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions) {
+export function addEventListener(dom: Window | Document | HTMLElement, type: string, cb: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions) {
     dom.addEventListener(type, cb, options);
     return () => dom.removeEventListener(type, cb, options);
 }
